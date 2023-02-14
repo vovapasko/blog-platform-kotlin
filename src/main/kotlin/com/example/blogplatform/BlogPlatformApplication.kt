80fc0b478932237b05fc93652c3241f8aa541855
vovapasko <vovapasko1699@gmail.com>
2023-02-14 15:16:30 +0100

    provided BlogProperties and HttpContrelrs

--- src/main/kotlin/com/example/blogplatform/BlogPlatformApplication.kt
+++ src/main/kotlin/com/example/blogplatform/BlogPlatformApplication.kt
@@ -1,9 +1,11 @@
 package com.example.blogplatform
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(BlogProperties::class)
 class BlogPlatformApplication
 
 fun main(args: Array<String>) {
