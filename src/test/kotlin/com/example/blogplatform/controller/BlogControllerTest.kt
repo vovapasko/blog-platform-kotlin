8e764ba42a3bc2925438746359e03fb54201a58b
vovapasko <vovapasko1699@gmail.com>
2023-02-14 12:02:27 +0100

    added Extensions and some tests to main test case

--- src/test/kotlin/com/example/blogplatform/controller/BlogControllerTest.kt
+++ src/test/kotlin/com/example/blogplatform/controller/BlogControllerTest.kt
@@ -1,6 +1,8 @@
 package com.example.blogplatform.controller
 
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -13,11 +15,26 @@ import org.springframework.http.HttpStatus
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BlogControllerTest(@Autowired val restTemplate: TestRestTemplate) {
 
+    @BeforeAll
+    fun setup(){
+        println(">> Setup")
+    }
+
     @Test
     fun `Asser get blog`(){
         val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).isEqualTo("<h1>Blog</h1>")
+        assertThat(entity.body).contains("<h1>Blog</h1>")
+    }
+
+    @Test
+    fun `Assert article page title, content and status code`() {
+        println(">> TODO")
+    }
+
+    @AfterAll
+    fun teardown(){
+        println(">> Tear down")
     }
 
 }
