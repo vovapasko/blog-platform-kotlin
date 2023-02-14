5275d5ff2ca1e3c5a525fc230bac3ec40ded9c0b
vovapasko <vovapasko1699@gmail.com>
2023-02-14 13:03:46 +0100

    tests don't work, need to fix them

--- src/main/kotlin/com/example/blogplatform/Entities.kt
+++ src/main/kotlin/com/example/blogplatform/Entities.kt
@@ -14,8 +14,7 @@ class Article(
     @ManyToOne var author: User,
     var slug: String = title.toSlug(),
     var addedAt: LocalDateTime = LocalDateTime.now(),
-    @Id @GeneratedValue var id: Long? = null
-)
+    @Id @GeneratedValue var id: Long? = null)
 
 @Entity
 class User(
@@ -23,5 +22,4 @@ class User(
     var firstname: String,
     var lastname: String,
     var description: String? = null,
-    @Id @GeneratedValue var id: Long? = null
-)
+    @Id @GeneratedValue var id: Long? = null)
