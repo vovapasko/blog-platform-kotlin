4c0c1d646156d9c47e6e60aa2c1cf6c59fe2ebd5
vovapasko <vovapasko1699@gmail.com>
2023-02-14 11:47:41 +0100

    provided template for get blogs and simple model

--- src/main/kotlin/com/example/blogplatform/controller/BlogController.kt
+++ src/main/kotlin/com/example/blogplatform/controller/BlogController.kt
@@ -1,23 +1,18 @@
 package com.example.blogplatform.controller
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
+
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-@RequestMapping(value = ["/blogs"])
 class BlogController {
 
-    @RequestMapping(method = [RequestMethod.GET])
-    fun getBlog() = "Hello world"
-
-    @RequestMapping(method = [RequestMethod.POST])
-    fun createBlog() = "Create blog"
-
-    @RequestMapping(method = [RequestMethod.PUT])
-    fun updateBlog() = "Update blog"
-
-    @RequestMapping(method = [RequestMethod.DELETE])
-    fun deleteBlog() = "Delete blog"
+    @GetMapping("/")
+    fun getBlog(model: Model): String {
+        model["title"] = "Blog"
+        return "blog"
+    }
 }
