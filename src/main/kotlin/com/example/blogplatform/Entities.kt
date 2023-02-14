a7342d71813294694583464126e22d93c517db71
vovapasko <vovapasko1699@gmail.com>
2023-02-14 12:32:30 +0100

    added entities

---
+++ src/main/kotlin/com/example/blogplatform/Entities.kt
@@ -0,0 +1,27 @@
+package com.example.blogplatform
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import java.time.LocalDateTime
+
+@Entity
+class Article(
+    var title: String,
+    var headline: String,
+    var content: String,
+    @ManyToOne var author: User,
+    var slug: String = title.toSlug(),
+    var addedAt: LocalDateTime = LocalDateTime.now(),
+    @Id @GeneratedValue var id: Long? = null
+)
+
+@Entity
+class User(
+    var login: String,
+    var firstName: String,
+    var lastName: String,
+    var description: String,
+    @Id @GeneratedValue var id: Long? = null
+)
