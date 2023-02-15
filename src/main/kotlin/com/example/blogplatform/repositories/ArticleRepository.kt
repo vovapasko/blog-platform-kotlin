3ad6499b4f4e197619e7c9041358b2714660235a
vovapasko <vovapasko1699@gmail.com>
2023-02-15 11:04:15 +0100

    feat: provided delete function in repository

--- src/main/kotlin/com/example/blogplatform/repositories/ArticleRepository.kt
+++ src/main/kotlin/com/example/blogplatform/repositories/ArticleRepository.kt
@@ -2,9 +2,11 @@ package com.example.blogplatform.repositories
 
 import com.example.blogplatform.models.Article
 import org.springframework.data.repository.CrudRepository
+import org.springframework.transaction.annotation.Transactional
 
-
-interface ArticleRepository: CrudRepository<Article, Long> {
+@Transactional
+interface ArticleRepository : CrudRepository<Article, Long> {
     fun findBySlug(slug: String): Article?
     fun findByOrderByAddedAtDesc(): Iterable<Article>
+    fun deleteArticleById(id: Long)
 }
--- src/main/kotlin/com/example/blogplatform/rest/ArticleController.kt
+++ src/main/kotlin/com/example/blogplatform/rest/ArticleController.kt
@@ -3,6 +3,7 @@ package com.example.blogplatform.rest
 
 import com.example.blogplatform.repositories.ArticleRepository
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -20,5 +21,8 @@ class ArticleController(private val repository: ArticleRepository) {
     fun findOne(@PathVariable slug: String) = repository.findBySlug(slug) ?: throw ResponseStatusException(
         HttpStatus.NOT_FOUND, "This article does not exist"
     )
+
+    @DeleteMapping("/{id}")
+    fun deleteOne(@PathVariable id: String) = repository.deleteArticleById(id.toLong())
 
 }
